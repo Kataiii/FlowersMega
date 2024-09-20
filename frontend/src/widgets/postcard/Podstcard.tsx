c83c749af8da8af037fe0371f7a2f5e4b902b26c
lmxd <>
2024-09-20 08:21:04 +0400

    add postcard block

--- frontend/src/widgets/postcard/Podstcard.tsx
+++ frontend/src/widgets/postcard/Podstcard.tsx
@@ -3,25 +3,36 @@ import { Input } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import { useState } from "react"
 
-
 interface PostcardProps {
-    onChange?: (position: number) => void;
+    value: any;
+    index: number;
+    onChange: (updatedVariation: any) => void;
+    onRemove: () => void;
 }
 
-const Podstcard: React.FC = () => {
+const Podstcard: React.FC<PostcardProps> = ({ value, onChange, onRemove, index }) => {
     const [position, setPosition] = useState();
+
+
 
     return (
         <div style={{ display: "flex", flexDirection: "column", width: "100%", border: "1px solid var(--primary-bg-color)", borderRadius: "8px", padding: "10px", gap: "10px" }}>
             <div style={{ display: 'flex', flexDirection: "row", justifyContent: "space-between" }}>
                 <p style={{ fontFamily: "Inter", fontSize: "16px", fontWeight: 600, color: "var(--text-modal)" }}>
-                    Открытка
+                    Открытка №{index}
                 </p>
-                <div style={{ width: "20px", height: "20px", padding: "2.5px", backgroundColor: "var(--primary-bg-color)", borderRadius: "4px" }}>
+                <div
+                    style={{ width: "20px", height: "20px", padding: "2px", backgroundColor: "var(--primary-bg-color)", borderRadius: "4px" }}
+                    onClick={onRemove}
+                >
                     <CloseOutlined />
                 </div>
             </div>
-            <Input.TextArea maxLength={200} />
+            <Input.TextArea
+                style={{ resize: "none", maxHeight: "110px", height: "110px" }}
+                onChange={(e) => onChange({ ...value, postcardText: e.target.value })}
+                value={value.postcardText || ""}
+                maxLength={200} />
         </div>
 
     )
