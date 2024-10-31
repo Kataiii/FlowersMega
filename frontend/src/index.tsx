d804f5d7cf64a5890d316f22f1b1413a4833e100
katya <katya_yuneva@mail.ru>
2024-10-31 21:30:10 +0400

    fix styles for build

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,33 +6,101 @@ import { ConfigProvider, Spin } from 'antd';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { createGlobalStyle, styled } from 'styled-components';
+
+const RootContainer = createGlobalStyle`
+  * {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  }
+
+  div::-webkit-scrollbar {}
+
+  div::-webkit-scrollbar-track {
+    background: #F5EFF5;
+    border-radius: 8px;
+  }
+
+  div::-webkit-scrollbar-thumb {
+    background-color: #FF749F;
+    border-radius: 8px;
+  }
+
+  div::-webkit-scrollbar-thumb:hover {
+    background: #FF558A;
+
+  }
+
+  div::-webkit-scrollbar {
+    height: 9px;
+    width: 5px;
+  }
+
+  .customSizedUpload .ant-upload {
+    width: 150px !important;
+    height: 155px !important;
+  }
+
+  textarea::-webkit-scrollbar-thumb {
+    background-color: #FF749F !important;
+    border-radius: 8px !important;
+  }
+
+  textarea::-webkit-scrollbar-thumb:hover {
+    background: #FF558A;
+  }
+
+  textarea::-webkit-scrollbar-track {
+    background: #F5EFF5;
+    border-radius: 8px;
+  }
+
+  .inputModal:disabled {
+    background-color: white !important;
+    color: black;
+  }
+
+  .inputModal::-webkit-scrollbar-thumb {
+    background-color: #FF749F !important;
+    border-radius: 8px !important;
+  }
+
+  textarea::-webkit-scrollbar-thumb:hover ::-webkit-scrollbar-thumb {
+    background-color: #FF749F !important;
+    border-radius: 8px !important;
+  }
+`;
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
-	<ConfigProvider
-		theme={{
-			token: {
-				colorPrimary: "#FF749F",
-				colorPrimaryActive: "#FF558A",
-				colorPrimaryHover: "#FF558A"
-			},
-			components: {
-				Input:{
-					activeBorderColor: "#FF558A",
-					hoverBorderColor: "#FF558A",
-					colorBorder: "#FF749F",
-					fontFamily: "Inter"
+	<>
+		<RootContainer/>
+		<ConfigProvider
+			theme={{
+				token: {
+					colorPrimary: "#FF749F",
+					colorPrimaryActive: "#FF558A",
+					colorPrimaryHover: "#FF558A"
+				},
+				components: {
+					Input:{
+						activeBorderColor: "#FF558A",
+						hoverBorderColor: "#FF558A",
+						colorBorder: "#FF749F",
+						fontFamily: "Inter"
+					}
 				}
-			}
-		}}>
-		<Provider store={store}>
-			<PersistGate persistor={persistor} loading={<Spin></Spin>}>
-				<BrowserRouter>
-					<Router />
-				</BrowserRouter>
-			</PersistGate>
-		</Provider>
-	</ConfigProvider>
+			}}>
+			<Provider store={store}>
+				<PersistGate persistor={persistor} loading={<Spin></Spin>}>
+					<BrowserRouter>
+						<Router />
+					</BrowserRouter>
+				</PersistGate>
+			</Provider>
+		</ConfigProvider>
+	</>
 );
