85d06def03d57f56496dc900c5e93e2ac47a6d61
lmxd <>
2024-12-17 19:01:49 +0400

    added additions for order

--- frontend/src/widgets/product/DescriptionProduct.tsx
+++ frontend/src/widgets/product/DescriptionProduct.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 import { isInCartSelector } from "../../entities/cart/redux/selectors";
 import { addOneToCart } from "../../entities/cart/redux/slice";
@@ -10,6 +10,14 @@ import { useAppDispatch, useAppSelector } from "../../store/store";
 import ts from "typescript";
 import { ignore } from "antd/es/theme/useToken";
 import PostcardAddBlock from "../postcard/PostcardAddBlock";
+import { Modal } from "antd";
+import ModalRoute from "../../shared/ui/modalRoute/ModalRoute";
+import ModalEmpty from "../../shared/ui/modalEmpty/ModalEmpty";
+import { SetStateAction, useState } from "react";
+import Additions from "./Additions";
+import DecorsAddition from "./DecorsAddition";
+import CardCategory from "../../entities/category/ui/cardCategory/CardCategory";
+import { useCategoriesControllerGetByIdQuery } from "../../store/category";
 
 type DescriptionProductProps = {
     product: Product & { productSize: ProductSize };
@@ -34,14 +42,21 @@ const DescriptionProduct: React.FC<DescriptionProductProps> = ({ product }) => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const isInCart = useAppSelector(state => isInCartSelector(state, product.productSize));
-    const { data: categoryIdData } = useCategoryControllerGetIdByNameQuery({ name: "Открытки" });
+    const { data: categoryIdData, isLoading: isCategoryDataLoading } = useCategoryControllerGetIdByNameQuery({ name: "Открытки" });
+    const location = useLocation();
+    const { data: categoryIdDatA } = useCategoryControllerGetIdByNameQuery({ name: "Мягкие игрушки" });
+    const { data: categoryIdDataB } = useCategoryControllerGetIdByNameQuery({ name: "Шары" });
+    const { data: categoryIdDataC } = useCategoryControllerGetIdByNameQuery({ name: "Топперы" });
+    const { data: ToppersAddition } = useCategoriesControllerGetByIdQuery({ id: Number(categoryIdDataC) });
+    const { data: DecorsAdditionCat } = useCategoriesControllerGetByIdQuery({ id: Number(categoryIdDatA) });
+    const { data: PostcardAddition } = useCategoriesControllerGetByIdQuery({ id: Number(categoryIdData) });
+
+    console.log(location)
     // @ts-ignore
-    console.log(product.categories[0].id)
-    // @ts-ignore
-    console.log(product.categories[0].id === Number(categoryIdData), "66666666666666666666666666")
+    // console.log(product.categories[0].id === Number(categoryIdData), "66666666666666666666666666")
     return (
         <>
-            {console.log(product.productSize.id, 'WHFUIWHFIUWHIUHWIUFHWUIHFUIWH')}
+
             {isLoading ? (
                 <p>Загрузка...</p>
             ) : (
@@ -57,45 +72,82 @@ const DescriptionProduct: React.FC<DescriptionProductProps> = ({ product }) => {
                     </div>
 
                 ) : (
-                    <div style={{ height: "fit-content", width: "50%", backgroundColor: "var(--block-bg-color)", padding: "24px 16px 50px", borderRadius: 6, display: "flex", flexDirection: "column", gap: 24 }}>
-                        <div style={{ display: "flex", gap: 16 }}>
-                            {data && data?.sizes.map((item, index) => (
-                                <Button
-                                    key={index}
-                                    $primary={product.productSize.idSize === item.id}
-                                    onClick={() => navigate(`${PRODUCT_PATH}/${product.name}/${item.name}`, { state: { idProduct: product.id, idSize: item.id } })}
-                                >
-                                    {item.name}
-                                </Button>
-                            ))}
-                        </div>
-                        <p style={{ fontFamily: "Inter", fontWeight: 600, fontSize: 24, color: "var(--secondary-text-color)" }}>
-                            {product.productSize.extraPrice.toLocaleString()} ₽
-                        </p>
-                        <p style={{ fontFamily: "Inter", fontWeight: 400, fontSize: 16, color: "var(--secondary-text-color)" }}>
-                            Размер букета {product.productSize.paramsSize}
-                        </p>
+                    <>
 
-                        <div style={{ display: "flex", gap: 10 }}>
-                            {isInCart ? (
-                                <ProductPageCountController id={product.productSize.id ?? -1} />
-                            ) : (
-                                <button onClick={() => dispatch(addOneToCart({ ...product.productSize, product }))} style={{ width: "49%", cursor: "pointer", minWidth: 380, height: 55, backgroundColor: "var(--primary-bg-color)", border: "1px solid #FF749F", borderRadius: 6, fontFamily: "Inter", fontWeight: 400, fontSize: 20, color: "var(--primary-text-color)" }}>
-                                    Заказать
+                        <div style={{ height: "fit-content", width: "50%", backgroundColor: "var(--block-bg-color)", padding: "24px 16px 50px", borderRadius: 6, display: "flex", flexDirection: "column", gap: 24 }}>
+                            <div style={{ display: "flex", gap: 16 }}>
+                                {/* @ts-ignore */}
+                                {(data?.sizes.length > 1) ? data?.sizes.map((item, index) => (
+                                    <Button
+                                        key={index}
+                                        $primary={product.productSize.idSize === item.id}
+                                        onClick={() => navigate(`${PRODUCT_PATH}/${product.name}/${item.name}`, { state: { idProduct: product.id, idSize: item.id } })}
+                                    >
+                                        {item.name}
+                                    </Button>
+                                )) : (null)}
+                            </div>
+                            <p style={{ fontFamily: "Inter", fontWeight: 600, fontSize: 24, color: "var(--secondary-text-color)" }}>
+                                {product.productSize.extraPrice.toLocaleString()} ₽
+                            </p>
+                            {
+                                // @ts-ignore
+                                product.categories[0].id === Number(categoryIdDataC) ||
+                                    // @ts-ignore
+                                    product.categories[0].id === Number(categoryIdDatA) ||
+                                    // @ts-ignore
+                                    product.categories[0].id === Number(categoryIdDataB) ? (
+                                    <p style={{ fontFamily: "Inter", fontWeight: 400, fontSize: 16, color: "var(--secondary-text-color)" }}>
+                                        Размер {product.productSize.paramsSize}
+                                    </p>
+                                ) : (
+                                    <p style={{ fontFamily: "Inter", fontWeight: 400, fontSize: 16, color: "var(--secondary-text-color)" }}>
+                                        Размер букета {product.productSize.paramsSize}
+                                    </p>
+                                )}
+
+
+                            <div style={{ display: "flex", gap: 10 }}>
+                                {isInCart ? (
+                                    <ProductPageCountController id={product.productSize.id ?? -1} />
+                                ) : (
+                                    <button onClick={() => dispatch(addOneToCart({ ...product.productSize, product }))} style={{ width: "49%", cursor: "pointer", minWidth: 380, height: 55, backgroundColor: "var(--primary-bg-color)", border: "1px solid #FF749F", borderRadius: 6, fontFamily: "Inter", fontWeight: 400, fontSize: 20, color: "var(--primary-text-color)" }}>
+                                        Заказать
+                                    </button>
+                                )}
+                                {/* {
+                                    // @ts-ignore
+                                    product.categories[0].id === Number(categoryIdDataC) ||
+                                        // @ts-ignore
+                                        product.categories[0].id === Number(categoryIdDatA) ||
+                                        // @ts-ignore
+                                        product.categories[0].id === Number(categoryIdDataB) ? (
+                                        null
+                                    ) : ( */}
+                                <button style={{ width: "49%", cursor: "pointer", minWidth: 380, height: 55, backgroundColor: "var(--block-bg-color)", border: "1px solid #FF749F", borderRadius: 6, fontFamily: "Inter", fontWeight: 400, fontSize: 20, color: "var(--primary-bg-color)" }}>
+                                    Купить в 1 клик
                                 </button>
-                            )}
-                            <button style={{ width: "49%", cursor: "pointer", minWidth: 380, height: 55, backgroundColor: "var(--block-bg-color)", border: "1px solid #FF749F", borderRadius: 6, fontFamily: "Inter", fontWeight: 400, fontSize: 20, color: "var(--primary-bg-color)" }}>
-                                Купить в 1 клик
-                            </button>
+                                {/* )} */}
+
+                            </div>
+
+                            <ListStructureProduct structure={product.structure} />
+                            <p style={{ fontFamily: "Inter", fontWeight: 300, fontSize: 16, color: "var(--secondary-text-color)" }}>
+                                {product.description}
+                            </p>
+
+
                         </div>
 
-                        <ListStructureProduct structure={product.structure} />
-                        <p style={{ fontFamily: "Inter", fontWeight: 300, fontSize: 16, color: "var(--secondary-text-color)" }}>
-                            {product.description}
-                        </p>
-                    </div>
+
+                        {/* <ToppersAddition isToppersOpen={isToppersOpen} setIsToppersOpen={setIsToppersOpen} />
+                        <PostcardAddition isPostcardsOpen={isPostcardsOpen} setIsPostcardsOpen={setIsPostcardsOpen} /> */}
+
+                    </>
                 )
             )}
+
+
         </>
     );
 };
